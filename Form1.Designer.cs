d3aca027fdd5eeb7056de1c2f31f4b6d047f2965
Not-Callum <callummarkjones@gmail.com>
2024-12-02 14:12:16 +0000

    Added Everything and more
    This is just a finished thing now

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TotalText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Pixeloid Sans", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(801, 82);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll-A-Dice";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Pixeloid Sans", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(166, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(442, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(72)))), ((int)(((byte)(17)))));
+            this.pictureBox2.Image = global::DiceRollingApp.Properties.Resources.Dices_Large_3;
+            this.pictureBox2.Location = new System.Drawing.Point(412, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(196, 184);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(72)))), ((int)(((byte)(17)))));
+            this.pictureBox1.Image = global::DiceRollingApp.Properties.Resources.Dices_Large_4;
+            this.pictureBox1.Location = new System.Drawing.Point(166, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TotalText
+            // 
+            this.TotalText.Font = new System.Drawing.Font("Pixeloid Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalText.ForeColor = System.Drawing.Color.Yellow;
+            this.TotalText.Location = new System.Drawing.Point(166, 281);
+            this.TotalText.Name = "TotalText";
+            this.TotalText.Size = new System.Drawing.Size(442, 46);
+            this.TotalText.TabIndex = 4;
+            this.TotalText.Text = "Total = ???";
+            this.TotalText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(90)))), ((int)(((byte)(22)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TotalText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TotalText;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,89 @@ namespace DiceRollingApp
 {
     public partial class Form1 : Form
     {
+
+        Random dice = new Random();
+
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int randNum1 = dice.Next(1,7);
+            int randNum2 = dice.Next(1, 7);
+
+            int totalNum1 = 0;
+            int totalNum2 = 0;
+
+            switch(randNum1)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Dices_Large_6;
+                    totalNum1 = 1;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Dices_Large_5;
+                    totalNum1 = 2;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Dices_Large_4;
+                    totalNum1 = 3;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Dices_Large_3;
+                    totalNum1 = 4;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.Dices_Large_2;
+                    totalNum1 = 5;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.Dices_Large_1;
+                    totalNum1 = 6;
+                    break;
+                default:
+                    pictureBox1.Image = Properties.Resources.Dices_Large_6;
+                    totalNum1 = 1;
+                    break;
+            }
+
+            switch(randNum2)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources.Dices_Large_6;
+                    totalNum2 = 1;
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources.Dices_Large_5;
+                    totalNum2 = 2;
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources.Dices_Large_4;
+                    totalNum2 = 3;
+                    break;
+                case 4:
+                    pictureBox2.Image = Properties.Resources.Dices_Large_3;
+                    totalNum2 = 4;
+                    break;
+                case 5:
+                    pictureBox2.Image = Properties.Resources.Dices_Large_2;
+                    totalNum2 = 5;
+                    break;
+                case 6:
+                    pictureBox2.Image = Properties.Resources.Dices_Large_1;
+                    totalNum2 = 6;
+                    break;
+                default:
+                    pictureBox2.Image = Properties.Resources.Dices_Large_6;
+                    totalNum2 = 1;
+                    break;
+            }
+
+            TotalText.Text = "Total = " + (totalNum1 += totalNum2).ToString();
         }
     }
 }
